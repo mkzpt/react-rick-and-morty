6985b22a01f91eef28f75e4d39a43e80bc35a853
Moises Perez <mkzperez.t@gmail.com>
2022-06-28 13:11:54 -0500

    Estilos

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export default function App() {
 
   
   return (
-    <div>
-      <h1>Rick and Morty</h1>
+    <div className='bg-dark text-white'>
+      <h1 className='text-center display-1 py-4'>Rick and Morty</h1>
       <CharacterList />
     </div>
   )
--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,31 +1,52 @@
-import React, {useState, useEffect} from 'react';
-import Character from './Character';
+import React, { useState, useEffect } from "react";
+import Character from "./Character";
 
 export default function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  const [loadin, setLoadin] = useState(true);
+  const [page, setPage] = useState(1);
 
-    const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+      );
+      const data = await response.json();
+      setCharacters(data.results);
+      setLoadin(false);
+    }
 
-    useEffect(() => {
-  
-      async function fetchData() {
-        const response = await fetch("https://rickandmortyapi.com/api/character");
-        const data = await response.json();
-        setCharacters(data.results);
-      }
-  
-      fetchData();
-  
-    }, [])
+    fetchData();
+  }, [page]);
 
   return (
-    <div>
-        {
-        characters.map(character => {
-          return(
-            <Character key={character.id} character={character}/>
-          )
-        })
-      }
+    <div className="container">
+      
+      <NavPage page={page} setPage={setPage} />
+
+      <div className="row">
+        {loadin ? <h1>Loading...</h1> : null}
+
+        {characters.map((character) => {
+          return (
+            <div className="col-md-4" key={character.id}>
+              <Character character={character} />
+            </div>
+          );
+        })}
+      </div>
+
+      <NavPage page={page} setPage={setPage} />
     </div>
-  )
+  );
 }
+
+
+function NavPage(props) {
+    return (
+      <header className="d-flex justify-content-between align-items-center">
+        <p>Page: {props.page}</p>
+        <button className="btn btn-primary btn-sm" onClick={() => props.setPage(props.page + 1)}>Page {props.page + 1}</button>
+      </header>
+    );
+  }
